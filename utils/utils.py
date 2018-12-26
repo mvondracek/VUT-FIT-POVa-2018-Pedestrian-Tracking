52b11825cb15855847fee9527b713700ec324056
xstast24 <stastny.F@seznam.cz>
2018-12-26 04:48:49 +0100

    image color&light sync: work in progress

--- utils/utils.py
+++ utils/utils.py
@@ -4,7 +4,95 @@ import cv2
 import numpy as np
 
 
-def synchronize_colors(image1, image2, mask1=None, mask2=None, interactive=False):  # TODO rename
+class RectangleSelectionCallback:
+    """Serves to select a rectangle in a cv2 image window using mouse. Implementation inspired by VUT FIT POVa du02.py."""
+    def __init__(self, image_width, image_height):
+        """
+        Width and height of the image is used to ignore coordinates larger than the image.
+        :param image_width: width of the image used for rectangle selection
+        :param image_height: height of the image used for rectangle selection
+        """
+        self._image_width = image_width
+        self._image_height = image_height
+        self._point1 = None
+        self._point2 = None
+        self.top_left = [0, 0]
+        self.bottom_right = [0, 0]
+        self.selection_active = False
+        self.selection_finished = False
+
+    def mouse_callback(self, event, x, y, flags, param):
+        # If the left mouse button is clicked, record the starting (x, y) coordinates and indicate that selection started.
+        if event == cv2.EVENT_LBUTTONDOWN:
+            self._point1 = self._point2 = (int(x), int(y))
+            self.selection_active = True
+            self._update_corners()
+        # If ongoing selection, update rectangle.
+        elif event == cv2.EVENT_MOUSEMOVE:
+            if self.selection_active:  # TODO
+                self._point2 = (int(x), int(y))
+                self._update_corners()
+        # Finish selection when left mouse button is released.
+        elif event == cv2.EVENT_LBUTTONUP:
+            if self.selection_active:
+                self._point2 = (int(x), int(y))
+                self.selection_active = False
+                self.selection_finished = True
+                self._update_corners()
+
+    def _update_corners(self):
+        """Calculate and update top-left and bottom-right corner coordinates."""
+        self.top_left[0] = max(0, min(self._point1[0], self._point2[0]))
+        self.top_left[1] = max(0, min(self._point1[1], self._point2[1]))
+        self.bottom_right[0] = min(self._image_width, max(self._point2[0], self._point2[0]))
+        self.bottom_right[1] = min(self._image_height, max(self._point2[1], self._point2[1]))
+
+
+def synchronize_images(image1, image2, mask1=None, mask2=None, interactive=False):  # TODO rename
+    """#TODO"""
+    # windows for interactive selection
+    window1 = None
+    window2 = None
+    if interactive:
+        image1_copy = image1.copy()
+        image2_copy = image2.copy()
+
+        window1 = 'image1_interactive_mask_selection'
+        window2 = 'image2_interactive_mask_selection'
+        cv2.namedWindow(window1, cv2.WINDOW_NORMAL)
+        cv2.namedWindow(window2, cv2.WINDOW_NORMAL)
+
+        rectangle1 = RectangleSelectionCallback(image1.shape[1], image1.shape[0])
+        rectangle2 = RectangleSelectionCallback(image2.shape[1], image2.shape[0])
+        cv2.setMouseCallback(window1, rectangle1.mouse_callback)
+        cv2.setMouseCallback(window2, rectangle2.mouse_callback)
+
+        # FIXME after moving to image rpovider uncomment: logger.info("Select a rectangle in both images, then press any key.")
+        cv2.imshow(window1, image1_copy)
+        cv2.imshow(window2, image2_copy)
+        cv2.waitKey()  # wait until the mask area is selected for both images
+        assert rectangle1.selection_finished and rectangle2.selection_finished, "Rectangle must be selected in both images!"
+
+        # TODO draw the rectangle
+
+        mask1 = np.zeros(image1.shape[0:2], dtype=np.uint8)  # mask needs to has one dimension less
+        mask2 = np.zeros(image2.shape[0:2], dtype=np.uint8)
+        mask1[rectangle1.top_left[1]:rectangle1.bottom_right[1]+1, rectangle1.top_left[0]:rectangle1.bottom_right[0]+1] = 1
+        mask2[rectangle2.top_left[1]:rectangle2.bottom_right[1]+1, rectangle2.top_left[0]:rectangle2.bottom_right[0]+1] = 1
+        # TODO add +1 to all subimage selections
+
+    image1, image2 = synchronize_images_colors(image1, image2, mask1=mask1, mask2=mask2, interactive=False)
+    image1, image2 = synchronize_images_light_intensity(image1, image2, mask1=mask1, mask2=mask2, interactive=False)
+
+    if interactive:
+        cv2.imshow(window1, image1)
+        cv2.imshow(window2, image2)
+        cv2.waitKey()
+
+    return image1, image2
+
+
+def synchronize_images_colors(image1, image2, mask1=None, mask2=None, interactive=False):  # TODO rename
     """#TODO"""
     # windows for interactive selection
     window1 = None
@@ -41,13 +129,8 @@ def synchronize_colors(image1, image2, mask1=None, mask2=None, interactive=False
     return image1, image2
 
 
-synchronize_colors(None, None, None, None, interactive=True)  # TODO
-
-def synchronize_light(image1, image2, mask1=None, mask2=None, interactive=False):  # TODO rename
+def synchronize_images_light_intensity(image1, image2, mask1=None, mask2=None, interactive=False):  # TODO rename
     """TODO"""
-    # TODO zkusit rectangle prumer, nebo prevest do jineho formatu a upravit jas - HSV / LAB
-    image1 = cv2.imread('../testing_data/s3_m_front_multi_y600.png')  # TODO Implement image provider.
-    image2 = cv2.imread('../testing_data/s3_f_side_multi_y600.png')  # TODO Implement image provider.
     if interactive:
         raise NotImplementedError  # TODO implement manual selection of image mask by mouse
         window1 = 'image1_interactive_mask_selection'
@@ -58,39 +141,37 @@ def synchronize_light(image1, image2, mask1=None, mask2=None, interactive=False)
         cv2.imshow(window2, image2)
         cv2.waitKey()
 
+    light_channel = 0
     image1 = cv2.cvtColor(image1, cv2.COLOR_BGR2LAB)
     image2 = cv2.cvtColor(image2, cv2.COLOR_BGR2LAB)
 
-    point1 = [(1180, 583), (1212, 597)]
-    point2 = [(129, 472), (163, 489)]
-
-    # mean only selected area
-    #mean1, _ = cv2.meanStdDev(image1[point1[0][1]:point1[1][1], point1[0][0]:point1[1][0]])
-    #mean2, _ = cv2.meanStdDev(image2[point1[0][1]:point1[1][1], point2[0][0]:point1[1][0]])
-
-    # mean whole image
-    mean1, _ = cv2.meanStdDev(image1)
-    mean2, _ = cv2.meanStdDev(image2)
-    #print(mean1)
-    #print(mean2)
+    # get mean value of lightness
+    mean1, _ = cv2.meanStdDev(image1[:, :, light_channel], mask=mask1)
+    mean2, _ = cv2.meanStdDev(image2[:, :, light_channel], mask=mask2)
+    ratio = mean1 / mean2
+    ratio = ratio.flatten()  # just 1 non-nested value
 
     image1 = image1.astype(np.float16)
     image2 = image2.astype(np.float16)
-    ratio_hue = mean1[0] / mean2[0]
-    print(ratio_hue)
-    image2[:, :, 0] = image2[:, :, 0] * ratio_hue
+    image2[:, :, light_channel] = image2[:, :, light_channel] * ratio
+    np.clip(image2, a_min=0, a_max=255, out=image2)
 
     image1 = image1.astype(np.uint8)
     image2 = image2.astype(np.uint8)
-
     image1 = cv2.cvtColor(image1, cv2.COLOR_LAB2BGR)
     image2 = cv2.cvtColor(image2, cv2.COLOR_LAB2BGR)
+
     if interactive:
         cv2.imshow(window1, image1)
         cv2.imshow(window2, image2)
         cv2.waitKey()
 
     return image1, image2
+
+
+image1 = cv2.imread('../testing_data/s3_m_front_multi_y600.png')  # TODO Implement image provider.
+image2 = cv2.imread('../testing_data/s3_f_side_multi_y600.png')  # TODO Implement image provider.
+synchronize_images(image1, image2, None, None, interactive=True)  # TODO
 
 
 def calculate_flat_histogram(image):
