68f77aaca9ef0c6f035038dc91e6db7e4f35e768
Martin Vondráček <vondracek.mar@gmail.com>
2018-12-28 01:45:27 +0100

    cs: image_provider.py

--- image_provider.py
+++ image_provider.py
@@ -7,19 +7,17 @@ BUT - Brno University of Technology
 """
 import logging
 from abc import ABC, abstractmethod
-from typing import List, Tuple
+from typing import Tuple
 
 import cv2
-import numpy as np
-
-from person import PersonTimeFrame, PersonView
-from utils import utils
 
 logger = logging.getLogger(__name__)
 
 
 class ImageProvider(ABC):
-    """ ImageProvider should provide pairs of front & side images. Image pairs should be provided in a chronological order. """
+    """
+    ImageProvider should provide pairs of front & side images. Image pairs should be provided in a chronological order.
+    """
     @abstractmethod
     def get_next_images(self) -> Tuple:
         """Load next available image pair (front image & side image)."""
@@ -29,6 +27,7 @@ class ImageProvider(ABC):
 class DummyImageProvider(ImageProvider):
     """Dummy provider provides the same predefined pair of images infinitely."""
     def __init__(self, front_image_path, side_image_path):
+        logger.debug('Using DummyImageProvider as ImageProvider.')
         self.front_image = cv2.imread(front_image_path)
         self.side_image = cv2.imread(side_image_path)
 
@@ -38,6 +37,7 @@ class DummyImageProvider(ImageProvider):
 
 class ImageProviderFromVideo(ImageProvider):
     def __init__(self, front_video_path, side_video_path):
+        logger.debug('Using ImageProviderFromVideo as ImageProvider.')
         self.front_video = cv2.VideoCapture(front_video_path)
         self.side_video = cv2.VideoCapture(side_video_path)
 
