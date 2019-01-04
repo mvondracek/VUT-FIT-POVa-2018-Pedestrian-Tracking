c3d1115916e61ce232b0d03bf20502f03dfa25ac
Martin Vondráček <vondracek.mar@gmail.com>
2019-01-04 04:06:31 +0100

    timer moved back to utils
    related: e2b012d76ec0d51c92e7301cab718ffe5d6ad4e6

--- utils/timer.py
+++ utils/timer.py
@@ -10,19 +10,19 @@ import timeit
 
 import cv2
 
-import detector
-import openpose
+from detector import OpenPoseBinaryDetector
+from openpose.PovaPose import PovaPose
 
 
 def povapose_single_image_single_person():
     """
     Measure pedestrian detection using PovaPose for single image with single person.
     """
-    person_detector = openpose.PovaPose.PovaPose(
-        prototxt_path="openpose/pose/coco/pose_deploy_linevec.prototxt",
-        caffemodel_path="openpose/pose/coco/pose_iter_440000.caffemodel"
+    person_detector = PovaPose(
+        prototxt_path="../openpose/pose/coco/pose_deploy_linevec.prototxt",
+        caffemodel_path="../openpose/pose/coco/pose_iter_440000.caffemodel"
     )
-    image = cv2.imread('testing_data/s2_f_x0y300.png')
+    image = cv2.imread('../testing_data/s2_f_x0y300.png')
 
     detection = ('person_detector.set_image_for_detection(image)\n'
                  'people = person_detector.run_multi_person_detection()\n')
@@ -40,9 +40,9 @@ def openpose_gpu_binary(openpose_binary_path=None, repeat=3):
     assert openpose_binary_path, 'Provide path to OpenPose binary!'
 
     # test single image
-    image = cv2.imread('testing_data/s2_f_x0y300.png')
+    image = cv2.imread('../testing_data/s2_f_x0y300.png')
 
-    person_detector = detector.OpenPoseBinaryDetector(openpose_binary_path, using_gpu=True)
+    person_detector = OpenPoseBinaryDetector(openpose_binary_path, using_gpu=True)
     detection = 'people = person_detector.detect(image, camera=None)\n'
     detection_time = timeit.repeat(detection,
                                    globals={'person_detector': person_detector, 'image': image},
@@ -67,9 +67,9 @@ def openpose_cpu_binary(openpose_binary_path=None, repeat=3):
     assert openpose_binary_path, 'Provide path to OpenPose binary!'
 
     # test single image
-    image = cv2.imread('testing_data/s2_f_x0y300.png')
+    image = cv2.imread('../testing_data/s2_f_x0y300.png')
 
-    person_detector = detector.OpenPoseBinaryDetector(openpose_binary_path, using_gpu=False)
+    person_detector = OpenPoseBinaryDetector(openpose_binary_path, using_gpu=False)
     detection = 'people = person_detector.detect(image, camera=None)\n'
     detection_time = timeit.repeat(detection,
                                    globals={'person_detector': person_detector, 'image': image},
